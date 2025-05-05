0947f8b26a45d6d7d133cf485bc1ffe824768afc
teddyfwillie <willieteddyf@gmail.com>
2025-05-05 12:06:55 +0530

    Add voice call interface, Real-time transcript display, End call/controls

--- app/(root)/interview/page.tsx
+++ app/(root)/interview/page.tsx
@@ -1,11 +1,13 @@
 import Agent from "@/components/Agent";
+import { getCurrentUser } from "@/lib/actions/auth.action";
 import React from "react";
 
-const page = () => {
+const page = async () => {
+  const user = await getCurrentUser();
   return (
     <>
       <h3>Interviews</h3>
-      <Agent userName="Your" userId="user1" type="generate" />
+      <Agent userName={user?.name!} userId={user?.id} type="generate" />
     </>
   );
 };
